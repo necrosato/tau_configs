908de1f19b2a29d94771eea3f387c5b27b13dbea
Naookie Sato <nsato@paratools.com>
2018-04-25 16:56:33 -0700

    update to dyninst

--- dyninst_instructions.sh
+++ dyninst_instructions.sh
@@ -1,6 +1,11 @@
 CPATH=$PWD
-#download boost
-BOOSTDIR=/home/naookiesato/Documents/tau_standalone/boost_1_67_0
+TAU_STANDALONE_DIR=/home/naookiesato/Documents/tau_standalone
+BOOSTTGZ=/home/naookiesato/Downloads/boost_1_67_0.tar.gz
+BOOSTDIR=$TAU_STANDALONE_DIR/boost_1_67_0
+#unzip boost
+cd $TAU_STANDALONE_DIR
+rm -rf $BOOSTDIR
+tar -zxvf $BOOSTTGZ
 cd $BOOSTDIR
 ./bootstrap.sh --prefix=boost_output
 ./b2 install -j4
@@ -8,9 +13,13 @@ mv boost_output/include .
 mv boost_output/lib .
 rmdir boost_output
 
+DYNINSTTGZ=/home/naookiesato/Downloads/dyninst-9.3.2.tar.gz
+DYNINSTSRCDIR=$TAU_STANDALONE_DIR/dyninst-9.3.2
+DYNINSTBUILDDIR=$TAU_STANDALONE_DIR/build-dyninst
 #unzip dyninst
-DYNINSTSRCDIR=/home/naookiesato/Documents/tau_standalone/dyninst-9.3.2
-DYNINSTBUILDDIR=/home/naookiesato/Documents/tau_standalone/build-dyninst
+cd $TAU_STANDALONE_DIR
+rm -rf $DYNINSTSRCDIR
+tar -zxvf $DYNINSTTGZ
 mkdir $DYNINSTBUILDDIR
 cd $DYNINSTBUILDDIR
 #cmake . -DBoost_INCLUDE_DIR=/home/naookiesato/Documents/tau_standalone/boost_1_67_0
