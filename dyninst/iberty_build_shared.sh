9b0967318b725684831ed180e66bf42e4e088dc3
Naookie Sato <nsato@paratools.com>
2018-05-08 15:44:43 -0700

    notes for iberty

--- dyninst/iberty_build_shared.sh
+++ dyninst/iberty_build_shared.sh
@@ -8,12 +8,15 @@ GCCSRCDIR=$TAU_STANDALONE_DIR/gcc
 IBERTYSRCDIR=$GCCSRCDIR/libiberty
 IBERTYPATH=$HOME/Downloads/libiberty.so
 cd $TAU_STANDALONE_DIR
-rm -rf $GCCSRCDIR
-git clone $GCCGIT
+#rm -rf $GCCSRCDIR
+#git clone $GCCGIT
 cd $IBERTYSRCDIR
 $IBERTYSRCDIR/configure --enable-shared --prefix=$IBERTYSRCDIR
-ld -G -Bdynamic pic/*.o -o libiberty.so
-cp libiberty.so $IBERTYPATH
+#ld -G -Bdynamic pic/*.o -o libiberty.so
+#ar rcs libiberty.a pic/*.o 
+#cp libiberty.so $IBERTYPATH
+make
+cp libiberty.a $IBERTYPATH
 
 cd $CPATH
-rm -rf $GCCSRCDIR
+#rm -rf $GCCSRCDIR
